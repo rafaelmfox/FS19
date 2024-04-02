7326953d8cf08de0a84c7953e938ad4657f09c4d
Rafael <rafaelmfox@gmail.com>
2024-04-02 07:43:38 -0300

    1
    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace FS19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNomeSave = new System.Windows.Forms.Label();
+            this.cbSaveGame = new System.Windows.Forms.ComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabFarm = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNomeSave
+            // 
+            this.lblNomeSave.AutoSize = true;
+            this.lblNomeSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeSave.Location = new System.Drawing.Point(12, 39);
+            this.lblNomeSave.Name = "lblNomeSave";
+            this.lblNomeSave.Size = new System.Drawing.Size(47, 16);
+            this.lblNomeSave.TabIndex = 0;
+            this.lblNomeSave.Text = "Save:";
+            // 
+            // cbSaveGame
+            // 
+            this.cbSaveGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.cbSaveGame.FormattingEnabled = true;
+            this.cbSaveGame.Location = new System.Drawing.Point(15, 12);
+            this.cbSaveGame.Name = "cbSaveGame";
+            this.cbSaveGame.Size = new System.Drawing.Size(715, 24);
+            this.cbSaveGame.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabFarm);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(15, 58);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(515, 412);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabFarm
+            // 
+            this.tabFarm.Location = new System.Drawing.Point(4, 22);
+            this.tabFarm.Name = "tabFarm";
+            this.tabFarm.Padding = new System.Windows.Forms.Padding(3);
+            this.tabFarm.Size = new System.Drawing.Size(507, 386);
+            this.tabFarm.TabIndex = 0;
+            this.tabFarm.Text = "Farm";
+            this.tabFarm.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(431, 386);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnCarregar.Location = new System.Drawing.Point(15, 472);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(97, 42);
+            this.btnCarregar.TabIndex = 3;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            this.btnCarregar.Click += new System.EventHandler(this.btnCarregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 527);
+            this.Controls.Add(this.btnCarregar);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.cbSaveGame);
+            this.Controls.Add(this.lblNomeSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNomeSave;
+        private System.Windows.Forms.ComboBox cbSaveGame;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabFarm;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnCarregar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace FS19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string directoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "My Games", "FarmingSimulator2019");
+            ListSaveGameFolders(directoryPath, cbSaveGame);
+            SortComboBoxNumerically2(cbSaveGame);
+
+        }
+
+
+        static void ListSaveGameFolders(string directoryPath, ComboBox comboBox)
+        {
+            try
+            {
+                string[] subDirectories = Directory.GetDirectories(directoryPath);
+
+                foreach (string subDirectory in subDirectories)
+                {
+                    if (subDirectory.ToLower().Contains("savegame"))
+                    {
+                        comboBox.Items.Add(subDirectory);
+                    }
+
+                    ListSaveGameFolders(subDirectory, comboBox);
+                }
+                comboBox.Sorted = true;
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine($"Acesso não autorizado: {e.Message}");
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"Diretório não encontrado: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ocorreu um erro: {e.Message}");
+            }
+        }
+
+        static void SortComboBoxNumerically2(ComboBox comboBox)
+        {
+            comboBox.Sorted = true; // Isso vai ajudar a ordernar numericamente
+        }
+
+        private void btnCarregar_Click(object sender, EventArgs e)
+        {
+
+            if (cbSaveGame.SelectedItem != null)
+            {
+                string selectedDirectory = cbSaveGame.SelectedItem.ToString();
+                LoadFarmXmlFile(selectedDirectory);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecione um diretório antes de carregar o arquivo.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+        }
+
+        public void LoadFarmXmlFile(string directoryPath)
+        {
+            try
+            {
+                string farmsXmlPath = Path.Combine(directoryPath, "farms.xml");
+
+                if (File.Exists(farmsXmlPath))
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(farmsXmlPath);
+
+                    // Acessar o primeiro nó <farm>
+                    XmlNode farmNode = xmlDoc.SelectSingleNode("/farms/farm");
+
+                    if (farmNode != null)
+                    {
+                        // Pegar os valores dos atributos 'name' e 'farmId'
+                        string farmName = farmNode.Attributes["name"].Value;
+                        string farmId = farmNode.Attributes["farmId"].Value;
+
+                        // Exibir os valores na label
+                        lblNomeSave.Text = "Nome da Fazenda: " + farmName;
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nenhuma fazenda encontrada no arquivo farms.xml.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("O arquivo farms.xml não foi encontrado.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro ao carregar o arquivo farms.xml: {ex.Message}");
+            }
+        }
+
+
+
+
+
+
+
+    }//fim
+}
